7efafcd8ad3380ac8e3fa7a7b65a108cd91f18b8
Manuel Becker <mabecker@uc.cl>
2018-03-30 18:44:56 -0300

    Fixing bugs

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 20180330185449) do
   end
 
   create_table "noticia", force: :cascade do |t|
-    t.string   "titulo"
+    t.string   "titulo"    
+    t.string   "bajada"
     t.text     "contenido"
     t.integer  "id_user"
     t.integer  "comentario"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "bajada"
   end
 
   create_table "users", force: :cascade do |t|
