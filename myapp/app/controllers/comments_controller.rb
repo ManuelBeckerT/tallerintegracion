bb853a4528eff64269cf85069fbeb9412803df72
Manuel Becker <mabecker@uc.cl>
2018-03-31 02:57:51 -0300

    Eliminar comentario implementado

--- myapp/app/controllers/comments_controller.rb
+++ myapp/app/controllers/comments_controller.rb
@@ -5,4 +5,11 @@ class CommentsController < ApplicationController
     redirect_to noticium_path(@noticium)
   end
 
+  def destroy
+      @noticium = Noticium.find(params[:noticium_id])
+      @comment = @noticium.comments.find(params[:id])
+      @comment.destroy
+
+      redirect_to noticium_path(@noticium)
+  end
 end
