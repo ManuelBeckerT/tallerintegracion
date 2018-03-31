9aa909c4643b7741262780f9065a8d5eaa489e82
Manuel Becker <mabecker@uc.cl>
2018-03-31 02:50:29 -0300

    Fixed bug with comments

--- myapp/app/controllers/comments_controller.rb
+++ myapp/app/controllers/comments_controller.rb
@@ -5,12 +5,4 @@ class CommentsController < ApplicationController
     redirect_to noticium_path(@noticium)
   end
 
-  def destroy
-    @noticium = Noticium.find(params[:noticium_id])
-    @noticium.comentario += 1
-    @comment = @noticium.comments.find(params[:id])
-    @comment.destroy
-    redirect_to post_path(@post)
-  end
-
 end
--- myapp/app/views/comentarios/_comentario.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! comentario, :id, :id, :id_user, :id_noticia, :texto, :titulo, :created_at, :updated_at
-json.url comentario_url(comentario, format: :json)
