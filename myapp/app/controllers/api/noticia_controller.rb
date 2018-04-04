d994e7f2535bc858b7e7b7d780d288ce275f1565
Manuel Becker <mabecker@uc.cl>
2018-04-04 15:23:42 -0300

    Avance API get one and all funcionan correctamente

--- myapp/app/controllers/api/noticia_controller.rb
+++ myapp/app/controllers/api/noticia_controller.rb
@@ -1,8 +1,45 @@
 class Api::NoticiaController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  before_action :set_noticium, only: [:show, :edit, :update, :destroy]
+  respond_to :json
 
   def index
     @noticia = Noticium.all.order("created_at DESC")
     render json: @noticia
   end
+
+  def new
+    @noticium = Noticium.new
+  end
+
+  def show
+    respond_with Noticium.find( params[:id] )
+  end
+
+  def create
+    @noticium = Noticium.new(noticium_params)
+    @noticium.comentario = 0
+    @noticium.id_user = current_user.id
+    respond_to do |format|
+      if @noticium.save
+        format.json { render :show, status: :created, location: @noticium }
+      else
+        format.json { render json: @noticium.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_noticium
+      @noticium = Noticium.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def noticium_params
+      params.require(:noticium).permit(:id, :titulo, :bajada, :contenido, :id_user, :comentario)
+    end
 
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
-  namespace :api do
+  namespace :api, defaults: { format: "json" } do
     resources :noticia, only: [:index, :create, :show, :update, :destroy]
 
   end
   get 'api/news/' => 'api/noticia#index'
+  post 'api/news/' => 'api/noticia#create'
+  get 'api/news/:id' => 'api/noticia#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
